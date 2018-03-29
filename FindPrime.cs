bbdf296d6531aced7483bc0d9d2eb2c9574e3eff
SonyJeng <sony2023@gmail.com>
2018-03-29 14:28:21 +0800

    change the file

---
+++ FindPrime.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region First
+            //Console.Write("請輸入一個數：");
+            //int input = int.Parse(Console.ReadLine());
+            //int counter = 0;
+            //for (int i = 2; i < input; i++)
+            //{
+            //    if (input % i == 0)
+            //    {
+            //        counter++;
+            //    }
+            //}
+            //Console.WriteLine(counter == 0 ? $"{input}是質數" : $"{input}不是質數");
+            #endregion
+
+            #region Second
+            //Console.Write("請輸入一個數：");
+            //int input = int.Parse(Console.ReadLine());
+            //int isPrime = 1;
+            //int i = 2;
+            //while (input / 2 != i)
+            //{
+            //    if (input % i == 0)
+            //    {
+            //        isPrime = 0;
+            //        break;
+            //    }
+            //    i++;
+            //}
+            //if (isPrime == 1)
+            //{
+            //    Console.WriteLine($"{input}是質數!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"{input}不是質數!");
+            //}
+            #endregion
+            Console.ReadKey();
+        }
+    }
+}
